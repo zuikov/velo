675cefb0660ffd7adf36cfe9b0ffed0dfc3e18bf
zuikov <andrei_z@ukr.net>
2018-05-28 22:46:05 +0300

    commented code was removed

--- src/app/wings/wings.component.ts
+++ src/app/wings/wings.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-// import { Config, ConfigService } from '../config/config.service';
-
 import { HttpClient} from '@angular/common/http';
 import { HttpService} from '../http.service';
 import { CartService } from '../cart/cart.service';
@@ -23,25 +21,17 @@ class Slide {
 })
 export class WingsComponent implements OnInit {
   slides = SLIDES;
-  wings: GoodItem[] =[];
-    
+  wings: GoodItem[] = [];
   constructor(private httpService: HttpService, private cartService: CartService) { }
-
-  // addGood(id): void {
-  //   this.cartService.goods = this.cartService.addGood(id);
-  //     console.log(this.cartService.goods);
-  // }
 
   setGood(id, name, price): void {
     let quantity = this.cartService.countGoodItem(id);
     this.cartService.addGoodItem(id, name, price, quantity);
-    this.cartService.announceCartCount("товар добавлен в корзину");
-      // console.log(this.cartService.goods);
+    this.cartService.announceCartCount('товар добавлен в корзину');
   }
 
   ngOnInit() {
-    this.httpService.getData('/assets/data/wings.json').subscribe(data => this.wings=data["wingList"]);
-    // console.log('this.wings', this.wings);
+    this.httpService.getData('/assets/data/wings.json').subscribe(data => this.wings=data['wingList']);
   }
 
 }
@@ -59,29 +49,5 @@ export const SLIDES: Slide[] = [
     alt: 'Baku subway',
     description: 'Card vending machines and turnstiles in Baku underground station'
   }
-  // {
-  //   url: '../../assets/images/hardware-projects/control-room-baku.jpg',
-  //   title: 'Control room in Baku subway',
-  //   alt: 'Control room in Baku subway',
-  //   description: 'Station operator work place'
-  // },
-  // {
-  //   url: '../../assets/images/hardware-projects/subway-erevan.jpg',
-  //   title: 'Erevan subway',
-  //   alt: 'Erevan subway',
-  //   description: 'Turnstiles in Erevan underground station'
-  // },
-  // {
-  //   url: '../../assets/images/hardware-projects/bus-ticket-validator.jpg',
-  //   title: 'Bus fare collection system',
-  //   alt: 'Bus fare collection system',
-  //   description: 'Validator for bus passengers'
-  // },
-  // {
-  //   url: '../../assets/images/hardware-projects/turnstiles-baku.jpg',
-  //   title: 'Turnstiles with flaps',
-  //   alt: 'Turnstiles with flaps',
-  //   description: 'Turnstiles in Baku underground station'
-  // },
-]
+];
 
